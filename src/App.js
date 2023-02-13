9f0f5209f0e7e403ed8493f3cdeb3810deea1a00
Eremeow138 <eremeow138@yandex.ru>
2023-02-13 08:26:03 +0300

    feat: get pizzas from mockapi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 /* eslint-disable react/jsx-key */
 import "./App.css";
 import "./scss/app.scss";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  useEffect(() => {
+    fetch("https://63e9ba4eb120461c6bf3deb0.mockapi.io/items")
+      .then((response) => response.json())
+      .then((json) => {
+        setPizzas(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
