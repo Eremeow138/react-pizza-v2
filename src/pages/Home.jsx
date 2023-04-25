677f487baf969b0bc8d6a33521950e570751da6f
Eremeow138 <eremeow138@yandex.ru>
2023-04-25 21:39:53 +0300

    feat: add cart markup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [pizzas, setPizzas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    window.scroll(0, 0);
     fetch("https://63e9ba4eb120461c6bf3deb0.mockapi.io/items")
       .then((response) => response.json())
       .then((json) => {
